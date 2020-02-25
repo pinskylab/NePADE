e76373f9c6bb2670486079708ed31347564a4f1a
jahoey <jahoey@users.noreply.github.com>
2020-02-25 14:22:40 -0500

    prep for plotting best fit model & parametric bootstraps

--- best_lhoods.R
+++ best_lhoods.R
@@ -38,8 +38,9 @@ stable.best.nomaf[which(stable.best.nomaf$MaxEstLhood == max(stable.best.nomaf$M
 stable.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/stable_instant_nomaf_Nlowerlimit100/best.lhood.summary.stable_nomaf.txt", header = TRUE)
 growing.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/growing_nomaf/best.lhood.summary.growing_nomaf_Nlowerlimit100.txt", header = TRUE)
 shrinking.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/shrinking_nomaf/best.lhood.summary.shrinking_nomaf.txt", header = TRUE)
-nobot.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/nobot_nomaf/best.lhood.summary.nobot_nomaf.txt", header = TRUE) # constant population size for comparision to all the bottlenecks
+# nobot.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/nobot_nomaf/best.lhood.summary.nobot_nomaf.txt", header = TRUE) # constant population size for comparision to all the bottlenecks
 nobot.best.nomaf2 <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/nobot_nomaf_oneparam/best.lhood.summary.nobot_nomaf.txt", header = TRUE) # constant population size for comparision to all the bottlenecks, but only estimating NPOP08
+tbot.fixed.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/instant_nomaf_fixedTBOT/best.lhood.summary.stable_nomaf_fixedTBOT.txt", header = TRUE)
 
 # Stable
 max(stable.best.nomaf$MaxEstLhood)
@@ -66,6 +67,11 @@ max(nobot.best.nomaf2$MaxEstLhood)
 nobot.best.nomaf2[which(nobot.best.nomaf2$MaxEstLhood == max(nobot.best.nomaf2$MaxEstLhood)),]
 nobot.best.nomaf2[5,]$MaxEstLhood - nobot.best.nomaf2[5,]$MaxObsLhood
 
+# Fixed TBOT to 7
+max(tbot.fixed.nomaf$MaxEstLhood)
+tbot.fixed.nomaf[which(tbot.fixed.nomaf$MaxEstLhood == max(tbot.fixed.nomaf$MaxEstLhood)),]
+tbot.fixed.nomaf[5,]$MaxEstLhood - tbot.fixed.nomaf[5,]$MaxObsLhood
+
 #### AIC calculations ####
 # a test
 # a <- c(-918.395, -687.045, -740.019, -782.598	) # MaxEstLhood
@@ -87,4 +93,38 @@ w <- vector()
 for (i in 1:length(aic)) {
   w[i] <- (exp(-0.5*(aic[i]-max(aic))))/sum(exp(-0.5*(aic[1]-max(aic))), exp(-0.5*(aic[2]-max(aic))), exp(-0.5*(aic[3]-max(aic))), exp(-0.5*(aic[4]-max(aic))))
 }
+
+#### Plot all fsc runs from parametric bootstrapping, plus the best fit model ####
+boot <- read.table() # Read in ML bootstrapped parameters
+stable.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/stable_instant_nomaf_Nlowerlimit100/best.lhood.summary.stable_nomaf.txt", header = TRUE)# Read in ML parameters from best model
+
+# Find maximum from best model
+max(stable.best.nomaf$MaxEstLhood)
+instant <- stable.best.nomaf[which(stable.best.nomaf$MaxEstLhood == max(stable.best.nomaf$MaxEstLhood)),]
+
+# Manipulate data so that its plottable
+# Best fit model
+max <- data.frame(matrix(NA, nrow = 6, ncol = 2))
+max[,1] <- c(2008, 2008-(2*instant$TBOT), 2008-(2*instant$TBOT), 2008-(2*instant$TBOT+instant$TLEN), 2008-(2*instant$TBOT+instant$TLEN), 1980)
+max[,2] <- c(instant$NPOP08, instant$NPOP08, instant$NBOT, instant$NBOT, instant$NANC, instant$NANC)
+
+# Parametric bootstrapped data
+boot.max <- array(numeric(), c(6,2,100))
+for (i in 1:nrow(boot)) {
+  boot.max[,1,i] <- c(2008, 2008-(2*boot$TBOT[i]), 2008-(2*boot$TBOT[i]), 2008-(2*boot$TBOT[i]+boot$TLEN[i]), 2008-(2*boot$TBOT[i]+boot$TLEN[i]), 1980)
+  boot.max[,2,i] <- c(boot$NPOP08[i], boot$NPOP08[i], boot$NBOT[i], boot$NBOT[i], boot$NANC[i], boot$NANC[i])
+}
+
+# Set up plot and plot bootstrapped data
+plot(max$X1, max$X2, xlab = 'Year', ylab = 'Effective population size', type = 'n', ylim = c(0,100000))
+for (l in 1:100) {
+  lines(jitter(boot.max[,1,l], factor = 0.3), boot.max[,2,l], col = 'gray90')
+}
+
+# for (l in 1:100) {
+#   lines(boot.max[,,l], col = 'gray90')
+# }
+
+# parameters from best fit model
+lines(max$X1, max$X2)
 
