13025e7b4c47024c32570f52f98a4aa97e6bce5c
Jennifer Hoey <jahoey@users.noreply.github.com>
2021-01-07 12:52:47 -0800

    replot boxplot and lines plots

--- best_lhoods.R
+++ best_lhoods.R
@@ -76,71 +76,93 @@ for (i in 1:length(aic)) {
 }
 
 #### Plot all fsc runs from nonparametric bootstrapping, plus the best fit model ####
-# Read in ML and bootstrapped parameters from instantaneous recovery model
-# boot <- read.table('~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/stable_instant_nomaf_Nlowerlimit100/max.summary.txt', header = TRUE) # Read in ML bootstrapped parameters following 50 runs for each simulated SFS
-# stable.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/stable_instant_nomaf_Nlowerlimit100/best.lhood.summary.stable_nomaf.txt", header = TRUE)# Read in ML parameters from instantaneous recovery model
+# Read in ML and nonparametric bootstrapped parameters from exponential growth of NANC, then bottleneck & instantaneous recovery model (Model 4)
+boot <- read.table('~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/model_results/model4.nonparametric.ci.summary.txt', header = TRUE) # Read in ML nonparametric bootstrapped parameters following 10 runs for each simulated SFS
+mod4 <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/model_results/model4.bestlhoods.summary.txt", header = TRUE) # Read in ML parameters from Model 4
 
-# Read in ML and nonparametric bootstrapped parameters from exponential growth of NANC, then bottleneck & instananeous recovery model
-boot <- read.table('~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/expgrowth_then_stable_instant_nomaf_Nlowerlimit100/nonparametric_ci_summary.txt', header = TRUE) # Read in ML nonparametric bootstrapped parameters following 10 runs for each simulated SFS
-expgrowth_instant.best.nomaf <- read.table("~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/from_amarel/expgrowth_then_stable_instant_nomaf_Nlowerlimit100/best.lhood.summary.expgrowth_instant_nomaf.txt", header = TRUE)# Read in ML parameters from instantaneous recovery model
+# Find maximum from best model
+# Exponential growth of NANC, then bottleneck & instantaneous recovery (Model 4)
+max(mod4$MaxEstLhood)
+mod4_ml_parameters <- mod4[which(mod4$MaxEstLhood == max(mod4$MaxEstLhood)),]
 
-# Find maximum from best models
-# Instantaneous recovery
-# max(stable.best.nomaf$MaxEstLhood)
-# instant <- stable.best.nomaf[which(stable.best.nomaf$MaxEstLhood == max(stable.best.nomaf$MaxEstLhood)),]
+#### Examine parameter point estimates and CIs ####
+# Histograms of parameters estimated from simulated SFS, plus a line for the point estimates
+hist(log10(boot$NPOP08), main = 'Population size in 2008', xlab = 'log10(NPOP08)')
+abline(v = log10(mod4_ml_parameters$NPOP08), col = 'tomato')
+hist(log10(boot$NANC), main = 'Ancestral population size', xlab = 'log10(NANC)')
+abline(v=log10(mod4_ml_parameters$NANC), col = 'tomato')
+hist(log10(boot$NBOT), main = 'Population size during bottleneck', xlab = 'log10(NBOT)')
+abline(v=log10(mod4_ml_parameters$NBOT), col = 'tomato')
+hist(boot$TBOT, main = 'Time after bottleneck', xlab = 'Generations (2 years/generation)')
+abline(v=mod4_ml_parameters$TBOT, col = 'tomato')
+hist(boot$TLEN, main = 'Length of bottleneck', xlab = 'Generations (2 years/generation)', breaks = 4)
+abline(v=mod4_ml_parameters$TLEN, col = 'tomato')
+hist(boot$RANC, main = 'NANC growth rate', xlab = 'Growth rate of NANC going back in time\n(Negative means positive growth)')
+abline(v=mod4_ml_parameters$RANC, col = 'tomato')
+hist(boot$MaxEstLhood)
+hist(boot$MaxObsLhood)
+hist(log10(boot$NBOT/boot$NPOP08), xlab = 'log10(NBOT/NPOP08)', main = 'NBOT/NPOP08 ratio') # same as hist(log10(boot$NBOT)-log10(boot$NPOP08))
+abline(v = log10(mod4_ml_parameters$NBOT/mod4_ml_parameters$NPOP08), col = 'tomato')
+hist(log10(boot$NANC/boot$NBOT), xlab = 'log10(NANC/NBOT)', main = 'NANC/NBOT ratio')
+abline(v = log10(mod4_ml_parameters$NANC/mod4_ml_parameters$NBOT), col = 'tomato')
 
-# Exponential growth of NANC, then bottleneck & instantaneous recovery
-max(expgrowth_instant.best.nomaf$MaxEstLhood)
-expgrowth_instant <- expgrowth_instant.best.nomaf[which(expgrowth_instant.best.nomaf$MaxEstLhood == max(expgrowth_instant.best.nomaf$MaxEstLhood)),]
+# Boxplots of population size from simulated SFS & point estimates of the ML parameters
+png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/Ne_boxplots.png",width=6, height=5, res=300, units="in")
+par(mar=c(4.5, 5, 1.5, 1), # panel magin size in "line number" units
+    mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
+    tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
+    cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
+    ps=12
+)
 
-#### Manipulate data so that its plottable ####
-# Instantaneous recovery model
-# max <- data.frame(matrix(NA, nrow = 6, ncol = 2))
-# max[,1] <- c(2008, 2008-(2*instant$TBOT), 2008-(2*instant$TBOT), 2008-(2*instant$TBOT+instant$TLEN), 2008-(2*instant$TBOT+instant$TLEN), 1980)
-# max[,2] <- c(instant$NPOP08, instant$NPOP08, instant$NBOT, instant$NBOT, instant$NANC, instant$NANC)
-# 
-# # Parametric bootstrapped data
-# boot.max <- array(numeric(), c(6,2,100))
-# for (i in 1:nrow(boot)) {
-#   boot.max[,1,i] <- c(2008, 2008-(2*boot$TBOT[i]), 2008-(2*boot$TBOT[i]), 2008-(2*boot$TBOT[i]+boot$TLEN[i]), 2008-(2*boot$TBOT[i]+boot$TLEN[i]), 1980)
-#   boot.max[,2,i] <- c(boot$NPOP08[i], boot$NPOP08[i], boot$NBOT[i], boot$NBOT[i], boot$NANC[i], boot$NANC[i])
-# }
-# 
-# # Set up plot and plot bootstrapped data
-# options(scipen = 5)
-# 
-# png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/instant_recovery_lineplot.png",width=6, height=5, res=300, units="in")
-# par(mar=c(4.5, 5, 1.5, 1), # panel magin size in "line number" units
-#     mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
-#     tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
-#     cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
-#     ps=12
-# )
-# 
-# # Plots bootstrapped 50 runs used to estimate 95% CI
-# plot(max$X1, max$X2, xlab = 'Year', ylab = expression('N'[e]), type = 'n', ylim = c(0,100000), las = 1)
-# for (l in 1:100) {
-#   lines(jitter(boot.max[,1,l], factor = 0.3), boot.max[,2,l], col = 'gray90')
-# }
-# 
-# # for (l in 1:100) {
-# #   lines(boot.max[,,l], col = 'gray90')
-# # }
-# 
-# # Plots parameters from best fit model
-# lines(max$X1, max$X2)
-# 
-# dev.off()
+# boxplot(log10(cis$NANC), log10(cis$NBOT), log10(cis$NPOP08), ylab = expression('log'[10]*'(N'[e]* 'estimate)'), names = c('NANC', 'NBOT', 'NPOP08')) # haploid
+# points(c(1,2,3), c(log10(best$NANC), log10(best$NBOT), log10(best$NPOP08)), col = 'tomato', pch = 19)
+# boxplot(log10(boot$NANC/2), log10(boot$NBOT/2), log10(boot$NPOP08/2), range = 0, ylab = expression('log'[10]*'(N'[e]* ' estimate)'), names = c('NANC', 'NBOT', 'NPOP08')) # convert Ne to diploid by dividing haploid number by 2
+boxplot(log10(boot$NANC/2), log10(boot$NBOT/2), log10(boot$NPOP08/2), range = 0, ylab = expression('log'[10]*'('*italic(N[e])* ' estimate)'), names = c('NANC', 'NBOT', 'NPOP08')) # convert Ne to diploid by dividing haploid number by 2; italic Ne
 
+points(c(1,2,3), c(log10(mod4_ml_parameters$NANC/2), log10(mod4_ml_parameters$NBOT/2), log10(mod4_ml_parameters$NPOP08/2)), col = 'tomato', pch = 19)
+
+dev.off()
+
+# Calculate 95% confidence intervals
+# NPOP08
+# mean(cis$NPOP08) + 1.960*(sd(cis$NPOP08)/sqrt(100))
+# mean(cis$NPOP08) - 1.960*(sd(cis$NPOP08)/sqrt(100))
+quantile(boot$NPOP08/2, c(0.025, 0.975)) # diploid
+
+# NANC
+quantile(boot$NANC/2, c(0.025, 0.975)) # diploid
+
+# NBOT
+quantile(boot$NBOT/2, c(0.025, 0.975)) # diploid
+
+# TBOT
+quantile(boot$TBOT, c(0.025, 0.975))
+
+# TLEN
+quantile(boot$TLEN, c(0.025, 0.975))
+
+# RANC
+quantile(boot$RANC, c(0.025, 0.975))
+
+# NPOP08/NBOT ratio
+mod4_ml_parameters$NPOP08/mod4_ml_parameters$NBOT # point estimate, same regardless of haploid or diploid
+quantile((boot$NPOP08/boot$NBOT), c(0.025, 0.975))
+
+# NBOT/NANC ratio
+mod4_ml_parameters$NBOT/mod4_ml_parameters$NANC # point estimate, same regardless of haploid or diploid
+quantile((boot$NBOT/boot$NANC), c(0.025, 0.975))
+
+#### Manipulate ML and bootstrapped data so that it can be plotted over time ####
 # Exponential growth of NANC then bottleneck & instantaneous recovery model & convert haploid numbers to diploid by dividing by 2; check RANC by calculating log(N_0/N_t)/(t_0/t_t)
 max <- data.frame(matrix(NA, nrow = 8, ncol = 4))
-max[,1] <- c(0, expgrowth_instant$TBOT, expgrowth_instant$TBOT, (expgrowth_instant$TBOT+expgrowth_instant$TLEN), (expgrowth_instant$TBOT+expgrowth_instant$TLEN), (expgrowth_instant$TBOT+expgrowth_instant$TLEN+2), (expgrowth_instant$TBOT+expgrowth_instant$TLEN+5), (expgrowth_instant$TBOT+expgrowth_instant$TLEN+1000)) #generations going back in time
-# max[,2] <- c(2008, 2008-(2*expgrowth_instant$TBOT), 2008-(2*expgrowth_instant$TBOT), 2008-((2*expgrowth_instant$TBOT)+(2*expgrowth_instant$TLEN)), 2008-((2*expgrowth_instant$TBOT)+(2*expgrowth_instant$TLEN)), 2008-((2*expgrowth_instant$TBOT)+(2*expgrowth_instant$TLEN))-10, 2008-((2*expgrowth_instant$TBOT)+(2*expgrowth_instant$TLEN))-20, 2008-((2*expgrowth_instant$TBOT)+(2*expgrowth_instant$TLEN))-30) #convert to years assuming summer flounder generation time of 2 years
+max[,1] <- c(0, mod4_ml_parameters$TBOT, mod4_ml_parameters$TBOT, (mod4_ml_parameters$TBOT+mod4_ml_parameters$TLEN), (mod4_ml_parameters$TBOT+mod4_ml_parameters$TLEN), (mod4_ml_parameters$TBOT+mod4_ml_parameters$TLEN+2), (mod4_ml_parameters$TBOT+mod4_ml_parameters$TLEN+5), (mod4_ml_parameters$TBOT+mod4_ml_parameters$TLEN+1000)) #generations going back in time
+# max[,2] <- c(2008, 2008-(2*mod4_ml_parameters$TBOT), 2008-(2*mod4_ml_parameters$TBOT), 2008-((2*mod4_ml_parameters$TBOT)+(2*mod4_ml_parameters$TLEN)), 2008-((2*mod4_ml_parameters$TBOT)+(2*mod4_ml_parameters$TLEN)), 2008-((2*mod4_ml_parameters$TBOT)+(2*mod4_ml_parameters$TLEN))-10, 2008-((2*mod4_ml_parameters$TBOT)+(2*mod4_ml_parameters$TLEN))-20, 2008-((2*mod4_ml_parameters$TBOT)+(2*mod4_ml_parameters$TLEN))-30) #convert to years assuming summer flounder generation time of 2 years
 max[,2] <- 2008 - 2*max[,1]
-max[,3] <- c(expgrowth_instant$NPOP08, expgrowth_instant$NPOP08, expgrowth_instant$NBOT, expgrowth_instant$NBOT, expgrowth_instant$NANC, (expgrowth_instant$NANC*exp(expgrowth_instant$RANC * 2)), (expgrowth_instant$NANC*exp(expgrowth_instant$RANC * 5)), (expgrowth_instant$NANC*exp(expgrowth_instant$RANC * 1000))) #haploid
+max[,3] <- c(mod4_ml_parameters$NPOP08, mod4_ml_parameters$NPOP08, mod4_ml_parameters$NBOT, mod4_ml_parameters$NBOT, mod4_ml_parameters$NANC, (mod4_ml_parameters$NANC*exp(mod4_ml_parameters$RANC * 2)), (mod4_ml_parameters$NANC*exp(mod4_ml_parameters$RANC * 5)), (mod4_ml_parameters$NANC*exp(mod4_ml_parameters$RANC * 1000))) #haploid
 max[,4] <- max[,3]/2 #diploid
 
-# Parametric bootstrapped data
+# Nonparametric bootstrapped data
 boot.max <- array(numeric(), c(8,4,100))
 for (i in 1:nrow(boot)) {
   boot.max[,1,i] <- c(0, boot$TBOT[i], boot$TBOT[i], (boot$TBOT[i]+boot$TLEN[i]), (boot$TBOT[i]+boot$TLEN[i]), (boot$TBOT[i]+boot$TLEN[i]+2), (boot$TBOT[i]+boot$TLEN[i]+5), (boot$TBOT[i]+boot$TLEN[i]+1000)) #generations going back in time
@@ -153,12 +175,14 @@ for (i in 1:nrow(boot)) {
 # Set up plot and plot bootstrapped data
 options(scipen = 5)
 
-png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/expgrowth_instant_lineplot.png",width=6, height=5, res=300, units="in")
-par(mar=c(4, 6, 1, 1), # panel margin size in "line number" units
+png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/model4_lineplot_a_and_b.png",width=6, height=10, res=300, units="in")
+# png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/model4_lineplot.png",width=6, height=5, res=300, units="in")
+par(mar=c(4, 6.5, 1.1, 1)+0.1, # panel margin size in "line number" units
     mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
     tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
     cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
-    ps=12
+    ps=12,
+    mfrow = c(2,1)
 )
 
 # Plots bootstrapped 50 runs used to estimate 95% CI. Specify haploid or diploid numbers
@@ -168,7 +192,8 @@ for (l in 1:100) {
   lines(jitter(boot.max[,2,l], factor = 0.2), boot.max[,4,l], col = cols)
 }
 mtext('Year', 1, 2.5, cex = 1.2)
-mtext(expression('N'[E]), 2, 3.7, cex = 1.2)
+mtext(expression(italic('N'[e])), 2, 3.7, cex = 1.2)
+mtext('(a)', 2,4.5, cex = 1.4, las = 1, at = 73000)
 
 # for (l in 1:100) {
 #   lines(boot.max[,,l], col = 'gray90')
@@ -177,15 +202,15 @@ mtext(expression('N'[E]), 2, 3.7, cex = 1.2)
 # Plots parameters from best fit model. Specify haploid or diploid
 lines(max$X2, max$X4, lwd = 1.8)
 
-dev.off()
+# dev.off()
 
-png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/expgrowth_instant_lineplot_deeptime.png",width=6, height=5, res=300, units="in")
-par(mar=c(4, 6, 1, 1), # panel margin size in "line number" units
-    mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
-    tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
-    cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
-    ps=12
-)
+# png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/NePADE/demo_modeling/model4_lineplot_deeptime.png",width=6, height=5, res=300, units="in")
+# par(mar=c(4, 6, 1, 1), # panel margin size in "line number" units
+#     mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
+#     tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
+#     cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
+#     ps=12
+# )
 
 # Plots bootstrapped 50 runs used to estimate 95% CI. Specify haploid or diploid
 plot(max$X2, max$X4, xlab = '', ylab = '', type = 'n', xlim = c(1500,2008), ylim = c(0,72000), las = 1)
@@ -193,7 +218,9 @@ for (l in 1:100) {
   lines(jitter(boot.max[,2,l], factor = 0.2), boot.max[,4,l], col = cols)
 }
 mtext('Year', 1, 2.5, cex = 1.2)
-mtext(expression('N'[E]), 2, 3.7, cex = 1.2)
+mtext(expression(italic('N'[e])), 2, 3.7, cex = 1.2)
+mtext('(b)', 2,4.5, cex = 1.4, las = 1, at = 73000)
+
 
 # Plots parameters from best fit model. Specify haploid or diploid
 lines(max$X2, max$X4, lwd = 1.8)
